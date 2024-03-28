23c91dd3f4a119fa483d4e48dbe6dc4dc909ea25
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-03-28 14:23:30 +0530

    created create new hotel and upadte existing hotel api

--- Routes/hotels.js
+++ Routes/hotels.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const Hotels = require('../Model/hotel.js')
+
+//create hotel
+router.post('/',async(req,res)=>{
+       const newHotel  = req.body
+       try {
+            await Hotels.create(newHotel)  
+            res.status(200)
+       } catch (error) {
+              res.status(500).json(error)
+       }
+})
 
 
-router.get('/',(req,res)=>{
-       res.send('hotels')
+//update hotel
+
+router.put('/:id',async(req,res)=>{
+       const updatedHotel = await  Hotels.findByIdAndUpdate(req.params.id,{$set : req.body})
+       try {
+            await Hotels.create(newHotel)  
+            res.status(200)
+       } catch (error) {
+              res.status(500).json(error)
+       }
 })
 
 
--- index.js
+++ index.js
@@ -7,25 +7,31 @@ const authRouter  = require('./Routes/auth.js')
 const userRouter  = require('./Routes/users.js')
 const adminRouter  = require('./Routes/admin.js')
 const hotelsRouter  = require('./Routes/hotels.js')
+const roomsRouter  = require('./Routes/rooms.js')
 
 
-const connect = async() =>{
-       try {
-              await mongoose.connect(process.env.MONGO)
-              console.log("DataBase connected");
-       } catch (error) {
-              console.log('Connection Failed');
-       }
-}
+//mongoDB connection function
+ try {
+       mongoose.connect(process.env.MONGO)
+       console.log("DataBase connected");
+} catch (error) {
+       console.log('Connection Failed');
+ }
 
 
+//middlewares 
+app.use(express.json())
+
+
+//Routes middlewares
 app.use('/auth',authRouter)
 app.use('/user',userRouter)
 app.use('/admin',adminRouter)
 app.use('/hotels',hotelsRouter)
+app.use('/rooms',roomsRouter)
+
 
 
 app.get('/',(req,res)=>{
-       connect();
        res.send('welcome');
 }).listen(3000)
