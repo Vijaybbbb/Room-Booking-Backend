ebeba02da4fefe6167a8b3c8d91962a0596e9e40
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-03-31 20:39:32 +0530

    otp

---
+++ Controller/users.js
@@ -0,0 +1,17 @@
+ const otpVerify = async() =>{
+       const email = req.query.email;
+
+       const hashedPassword = await bcrypt.hash(password, 10)
+              const newUser = {
+                     userName: username,
+                     email: email,
+                     password: hashedPassword
+              }
+              await User.create(newUser)
+              res.status(200).json({ message: 'User Hasbeen created' })
+ }
+
+
+ module.exports = {
+       otpVerify
+ }
--- Routes/users.js
+++ Routes/users.js
@@ -1,31 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const {verifyTocken}  = require('../utils/verifyTocken.js')
+const {otpVerify} = require('../Controller/users.js')
 
 
 router.get('/checkAuthentication',verifyTocken,(req,res)=>{
        res.send('users')
 })
 
-
-router.get('/otpVerify',async(req,res)=>{
-       const email = req.query.email;
-
-
-
-
-
-
-       const hashedPassword = await bcrypt.hash(password, 10)
-              const newUser = {
-                     userName: username,
-                     email: email,
-                     password: hashedPassword
-              }
-              await User.create(newUser)
-              res.status(200).json({ message: 'User Hasbeen created' })
-
-})
+router.get('/otpVerify',otpVerify)
 
 
 module.exports  = router
