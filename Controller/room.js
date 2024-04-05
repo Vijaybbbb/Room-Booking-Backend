da7196b110b9391db0388469f4ac5aaa7fe87cb1
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-04-05 18:49:16 +0530

    build rooms crud functions

--- Controller/room.js
+++ Controller/room.js
@@ -3,3 +3,72 @@ const Hotels  =  require('../Model/hotel')
 const {createError}  = require('../utils/error.js')
 
 
+const createRoom  = async (req,res,next) =>{
+
+       const hotelId = req.params.hotelId;
+       const newRoom  = new Room(req.body)
+       try{
+              const savedRoom = await newRoom.save()
+              try {
+                     await Hotels.findByIdAndUpdate(hotelId ,
+                            {$push : {rooms:savedRoom._id}})
+              } catch (error) {
+                      return next(createError(401,'Failed'))    
+              }
+              res.status(200).json(savedRoom)
+       }
+       catch(error){
+              return next(createError(401,'Failed'))
+       }
+}
+
+const updateRoom  = async (req,res,next) =>{
+
+       const hotelId = req.params.hotelId;
+       const newRoom  = new Room(req.body)
+       try{
+              const savedRoom = await newRoom.save()
+              try {
+                     await Hotels.findByIdAndUpdate(hotelId ,
+                            {$push : {rooms:savedRoom._id}},
+                            
+                     )
+              } catch (error) {
+                      return next(createError(401,'Failed'))    
+              }
+              res.status(200).json(savedRoom)
+       }
+       catch(error){
+              return next(createError(401,'Failed'))
+       }
+}
+
+
+const deleteRoom  = async (req,res,next) =>{
+
+       const hotelId = req.params.hotelId;
+       const newRoom  = new Room(req.body)
+       try{
+              const savedRoom = await newRoom.save()
+              try {
+                     await Hotels.findByIdAndUpdate(hotelId ,
+                            {$push : {rooms:savedRoom._id}})
+              } catch (error) {
+                      return next(createError(401,'Failed'))    
+              }
+              res.status(200).json(savedRoom)
+       }
+       catch(error){
+              return next(createError(401,'Failed'))
+       }
+}
+
+
+
+
+
+
+module.exports = {
+       createRoom,
+
+}
