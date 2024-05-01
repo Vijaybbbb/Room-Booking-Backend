b9619e4976a15f6cda4ed4e5fa5deba6b013e71b
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-05-01 22:41:43 +0530

    refscture

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,8 +1,6 @@
 const express = require('express')
-const User = require('../Model/user')
 const router = express.Router()
 const bcrypt = require('bcrypt')
-const jwt  = require('jsonwebtoken')
 const { adminLogin,
         adminHome,
         getAllUsers,
